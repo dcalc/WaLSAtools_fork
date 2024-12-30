d3b95aeac0cc389b90cbc1d42c8c6ea8b79179c0
Shahin Jafarzadeh <shahin.jafarzadeh@qub.ac.uk>
2024-12-30 09:10:21 +0100

    new configs

--- codes/python/setup.py
+++ codes/python/setup.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------------------------------------------------
 # WaLSAtools: Wave analysis tools
-# Copyright (C) 2025 WaLSA Team - Shahin Jafarzadeh
+# Copyright (C) 2025 WaLSA Team - Shahin Jafarzadeh et al.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 # Note: If you use WaLSAtools for research, please consider citing:
 # Jafarzadeh, S., Jess, D. B., Stangalini, M. et al. 2025, Nature Reviews Methods Primers, in press.
 # -----------------------------------------------------------------------------------------------------
-
 
 from setuptools import setup, find_packages
 
