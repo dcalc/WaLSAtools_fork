5d2c05df299e3c1af1563a6369576546d0236cda
Shahin Jafarzadeh <shahin.jafarzadeh@qub.ac.uk>
2025-01-03 14:51:54 +0100

    update

--- docs/theme/javascripts/walsatools-interactive_docs.js
+++ docs/theme/javascripts/walsatools-interactive_docs.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Select links from both md-nav and md-tabs
-    const navLinks = document.querySelectorAll('a.md-nav__link, a.md-tabs__link');
+    const navLinks = document.querySelectorAll('a.md-nav__link , a.md-tabs__link');
     navLinks.forEach(link => {
         link.addEventListener('click', event => {
             // Force a full page reload
